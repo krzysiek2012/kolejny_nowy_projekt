93398bbee539f58adacb7b02b4a98b680a2e7da1
krzysiek2012 <olszewski.krz@gmail.com>
2019-12-14 13:21:15 +0100

    pierwszy teks

---
+++ capital.py
@@ -0,0 +1,10 @@
+import unittest
+import upper
+
+class testy_litery(unittest.TestCase):
+    def test_upper(self):
+        font=upper.duze(jakis teks z malymi literami)
+        self.assertEqual(txt, Jakis Teks Z Malymi Literami)
+
+if __name__=='__main__':
+    unittest.main()
