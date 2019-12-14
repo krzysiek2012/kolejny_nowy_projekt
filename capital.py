f24cbc61974e311f0a9d81c5affadbcbaa9dd361
krzysiek2012 <olszewski.krz@gmail.com>
2019-12-14 14:05:56 +0100

    dodane pomniejszanie

--- capital.py
+++ capital.py
@@ -5,5 +5,12 @@ class Testy_litery(unittest.TestCase):
     def test_upper(self):
         font = upper.duze('jakis teks z malymi literami')
         self.assertEqual(font, 'Jakis teks z malymi literami')
+
+    def test_foldowanie(self):
+        font=fold.male('Jakas Dziwna Powiastka O Niczym')
+        self.assertEqual(font, jakas dziwna powiastka o niczym')
+        
+
+
 if __name__=='__main__':
     unittest.main()
