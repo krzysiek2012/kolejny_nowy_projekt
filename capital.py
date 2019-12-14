2e271c37d6083265230cab68d3ac6bcb0ff8af60
szwczk <szwczk.klaudia@gmail.com>
2019-12-14 14:58:56 +0100

    nowe poprawki

--- capital.py
+++ capital.py
@@ -4,6 +4,6 @@ import upper
 class Testy_litery(unittest.TestCase):
     def test_upper(self):
         font = upper.duze('jakis teks z malymi literami')
-        self.assertEqual(font, 'Jakis Teks Z Malymi Literami')
+        self.assertEqual(font, 'Jakis teks z malymi literami')
 if __name__=='__main__':
     unittest.main()
